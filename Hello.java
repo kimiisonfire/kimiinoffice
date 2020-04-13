537b00d79de1539720795cb6541159dea20f8062
kimiisonfire <kimiisonfire@gmail.com>
2020-04-13 14:25:57 +0900

    github desktop test2
    test for manual

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello {
 		String str = "modified again";
 		String str2 = "without add!";
 		String str3 = "undo test";
+		String str4 = "github desktop test";
 		System.out.println("hello git!");
 	}
 }
