2c50ad010409ec5f323421074c9f4a8318cef66e
kimiisonfire <kimiisonfire@gmail.com>
2020-04-13 13:55:39 +0900

    github desktop  test
    Dos github desktop work?

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 	public static void main(String[] args) {
 		String str = "modified again";
 		String str2 = "without add!";
+		String str3 = "undo test";
 		System.out.println("hello git!");
 	}
 }
