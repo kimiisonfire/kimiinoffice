f6c2148f053b39d37158e09ce017d1e22d94d15b
kimiisonfire <kimiisonfire@gmail.com>
2020-04-28 15:38:18 +0900

    in testing , remove conflict

--- Hello.java
+++ Hello.java
@@ -4,8 +4,5 @@ public class Hello {
 		String str2 = "without add!";
 		String str3 = "undo test";
 		String str4 = "github desktop test";
-		String str6 = "in testing branch , for conflict2";
-		String str7 = "in testing branch , for conflict3";
-		System.out.println("hello git!");
 	}
 }
