864630629355799e0c0b0cc96fae1f3679ae55e9
kimiisonfire <kimiisonfire@gmail.com>
2020-04-28 15:27:00 +0900

    in master , for conflict 2

--- Hello.java
+++ Hello.java
@@ -4,11 +4,7 @@ public class Hello {
 		String str2 = "without add!";
 		String str3 = "undo test";
 		String str4 = "github desktop test";
-<<<<<<< HEAD
 		String str5 = "in master branch , for merge conflict";
-=======
-		String str6 = "in testing branch , for merge conflict";
->>>>>>> testing
 		System.out.println("hello git!");
 	}
 }
