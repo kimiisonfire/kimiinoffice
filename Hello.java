56b7c469437d6bd78a7212c1370a3fb5d1b2ffb9
kimiisonfire <kimiisonfire@gmail.com>
2020-05-07 11:20:45 +0900

    rebase test - mod in iss01

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 		String str2 = "without add!";
 		String str3 = "undo test";
 		String str4 = "github desktop test";
+		String str5 = "rebase - iss01";
 		System.out.println("hello git!");
 	}
 }
